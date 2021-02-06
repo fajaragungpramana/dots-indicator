5e1e45d8f76d2b8c9da2ef2f645492dbc66f2802
fajaragungpramana <fajar.agungpramana01@gmail.com>
2021-02-06 20:51:59 +0700

    Code improvement. Change root to linear layout and dot list using view component.

--- dotsindicator/src/main/java/com/github/fajaragungpramana/dotsindicator/DotsIndicator.kt
+++ dotsindicator/src/main/java/com/github/fajaragungpramana/dotsindicator/DotsIndicator.kt
@@ -1,44 +1,119 @@
 package com.github.fajaragungpramana.dotsindicator
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
+import android.view.Gravity
+import android.view.View
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.ViewPager
+import java.lang.NullPointerException
 
-class DotsIndicator @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet?,
-    defStyle: Int = 0
-) : BaseDots(context, attrs, defStyle) {
+open class DotsIndicator(context: Context, attrs: AttributeSet?) : LinearLayout(context, attrs) {
 
-    override val dotStyle: DotStyle
-        get() = DotStyle.DOTS
+    protected val mDotList: ArrayList<View>
 
-    private val mDotContainer: LinearLayout
-        get() {
-            field.orientation = LinearLayout.HORIZONTAL
-            return field
-        }
+    protected val dotColor: Int
+
+    protected var dotCount: Int
+
+    protected val dotSelectedColor: Int
+
+    protected val dotSize: Float
+
+    protected val dotSpacing: Float
 
     var viewPager: ViewPager? = null
         set(value) {
             field = value
 
-            dotCount = field?.adapter?.count
-
-            mDotContainer.removeAllViews()
-            initDots(R.drawable.bg_dot) { dot, param -> mDotContainer.addView(dot, param) }
-
-            onDotSelected(field)
+            onPageSelected(field) { onPage(it) }
         }
 
     init {
-        mDotContainer = LinearLayout(context)
+        context.obtainStyledAttributes(attrs, R.styleable.DotsIndicator).also {
+            dotColor = it.getColor(R.styleable.DotsIndicator_dotColor, Color.LTGRAY)
+            dotCount = it.getInt(R.styleable.DotsIndicator_dotCount, 5)
+            dotSelectedColor = it.getColor(R.styleable.DotsIndicator_dotSelectedColor, Color.RED)
+            dotSize = it.getDimension(R.styleable.DotsIndicator_dotSize, 32F)
+            dotSpacing = it.getDimension(R.styleable.DotsIndicator_dotSpacing, 8F)
+        }.recycle()
 
-        initDots(R.drawable.bg_dot) { dot, param -> mDotContainer.addView(dot, param) }
-        onDotSelectedPosition(0)
+        mDotList = ArrayList()
 
-        addView(mDotContainer)
+        initLayout()
+        initDots()
+    }
+
+    private fun initLayout() {
+        gravity = Gravity.CENTER
+        orientation = HORIZONTAL
+    }
+
+    private fun initDots() {
+
+        repeat(dotCount) { i ->
+            val dot = View(context).also {
+                it.background = ContextCompat.getDrawable(context, R.drawable.bg_dot)
+            }
+
+            (dot.background as GradientDrawable).also {
+                it.setColor(dotColor)
+                it.cornerRadius = dotSize
+            }
+
+            val param = LayoutParams(dotSize.toInt(), dotSize.toInt()).also {
+                it.marginStart = dotSpacing.toInt() / 2
+                it.marginEnd = dotSpacing.toInt() / 2
+            }
+
+            mDotList.add(dot)
+
+            addView(mDotList[i], param)
+        }
+
+        onPage(0)
+    }
+
+    private fun onPageSelected(viewPager: ViewPager?, position: (Int) -> Unit) {
+        viewPager?.also {
+
+            if (it.adapter != null) {
+                dotCount = it.adapter?.count ?: 0
+
+                removeAllViews()
+                initDots()
+
+                it.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+                    override fun onPageScrollStateChanged(state: Int) {}
+
+                    override fun onPageScrolled(
+                        position: Int,
+                        positionOffset: Float,
+                        positionOffsetPixels: Int
+                    ) {
+                    }
+
+                    override fun onPageSelected(position: Int) {
+                        position(position)
+                    }
+
+                })
+
+            } else
+                throw NullPointerException("No adapter ViewPager attached!")
+        }
+    }
+
+    protected open fun onPage(position: Int) {
+        repeat(mDotList.size) {
+            (mDotList[it].background as GradientDrawable).setColor(
+                if (it == position) dotSelectedColor else dotColor
+            )
+        }
     }
 
 }
